a200905d42a7bef92b369834c0c0c074e6d23fff
GeovanniChaves <chavesgeovanni0@gmail.com>
2026-07-03 05:35:48 -0300

    feat: implementa cadastro de produtos no menu
    - Adiciona funcionalidade de cadastro de produtos
    - Permite inserir nome e valor do produto
    - Controla quantidade de produtos cadastrados com totalProdutos
    - Implementa limite máximo de 10 produtos
    - Permite cadastro contínuo até o usuário encerrar ou atingir o limite

--- CadastroShop/src/Loja.java
+++ CadastroShop/src/Loja.java
@@ -3,12 +3,20 @@ public class Loja {
 
 	public static void main(String[] args) {
 
-Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		
 		int escolha = 0;
+		int totalProdutos = 0;
+		boolean continuar = true;
 		
-		while(escolha != 5 /*&& escolha <=5*/){
+		String [] produto = new String [10];
+		double [] valor = new double [10];
+		
+		
+		while(escolha != 5){
+			
+			System.out.println("");
 			System.out.println("===== LOJA =====");
 			System.out.println("1 - Cadastrar produto");
 			System.out.println("2 - Listar produtos");
@@ -18,16 +26,87 @@ Scanner sc = new Scanner(System.in);
 
 			System.out.printf("%nEscolha uma opção: ");
 			
-			escolha = sc.nextInt();
-			
-		}
+			escolha = sc.nextInt();		
+			sc.nextLine();
 		
-		
-		
-		sc.close();
 
 		
-		
-	}
+			switch(escolha){
+				
+				case 1:
+					
+					continuar = true;	
+					
+					while(continuar && totalProdutos < produto.length){
+											
+						System.out.println("");
+						System.out.println("Nome do Produto: ");
+						produto[totalProdutos] = sc.nextLine();
+						
+						System.out.println("Valor do Produto: ");
+						valor[totalProdutos] = sc.nextDouble();
+						sc.nextLine();
+						
+						System.out.println("Cadastro realizado com sucesso!");
+						
+						System.out.println("Deseja Cadastrar Produto? (S/N)");
+						String resposta = sc.nextLine();
+						
+						if(resposta.equalsIgnoreCase("N")){										
+							continuar = false;					
+							System.out.println("Cadastro finalizado. Voltando ao menu...");
+							
+						}else {
+							System.out.println("Continuando cadastro...");
+							totalProdutos++;
+						}
+						
+						
+					}
+					
+					if(totalProdutos>=produto.length){
+						System.out.println("Limite de Produtos Excedido.");
+					}
+						
+					break;
+					
+					
+				case 2:
+					
+					break;
+					
+					
+				case 3:
+					
+					break;
+					
+					
+				case 4:
+					
+					break;
+					
+					
+				case 5:
+					
+					System.out.println("Saindo do Sistema.");
+					break;
+					
+					
+				default:
+					System.out.println("");
+					System.out.println("Escolha uma opção válida.");
+			
+			
+					
+			
+			}
+			
+		}
+			
+			
+			
+			sc.close();
+			
+		}
 
 }
